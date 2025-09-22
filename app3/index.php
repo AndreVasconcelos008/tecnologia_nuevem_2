24879b18bcaad7066c04675e971ef203527a598d
AndreVasconcelos008 <163846295+AndreVasconcelos008@users.noreply.github.com>
2025-09-22 07:38:38 -0300

    Create index.php

---
+++ app3/index.php
@@ -0,0 +1,14 @@
+<?php
+function stringParaBinario($string) {
+  $binario = '';
+  foreach(str_split($string) as $char) {
+    $binario .=str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT) . ' ';
+  }
+  return $binario;
+}
+$farse= "André Vasconcelos de Sá";
+$fraseBinario = stringParaBinario($frase);
+
+echo "Frase original: " . $frase . "<br>";
+echo "Frase em binário: " . $fraseBinario;
+?>
